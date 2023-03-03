de673026e9b63215d4d3f0067a2cd0aea0c3868e
sj_dev <sj94779@gmail.com>
2023-03-03 13:19:26 +0530

    check

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:get/get.dart';
 import 'package:my_learn_gadgets_web/core/app_colors.dart';
 import 'package:my_learn_gadgets_web/features/splash/ui/splash_page.dart';
-
 import 'features/unknown/unknown_route.dart';
 
 Future<void> main() async {
