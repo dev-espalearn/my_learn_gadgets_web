327e32616680c2ecbbb7a3d5d2395b0bcc5a19f7
sj_dev <sj94779@gmail.com>
2023-03-13 12:29:14 +0530

    ui changes

--- lib/features/profile/ui/profile_dashboard.dart
+++ lib/features/profile/ui/profile_dashboard.dart
@@ -26,91 +26,116 @@ class ProfileDashboard extends StatelessWidget {
           body: Padding(
             padding: const EdgeInsets.all(8.0),
             child:
-            Container(
-                width: Get.width,
-                height: Get.height,
-              child: GridView.builder(
-                gridDelegate:
-                const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 5.0,
-                  mainAxisSpacing: 5.0,
-                  childAspectRatio: 3
-                ),
-                // const SliverGridDelegateWithFixedCrossAxisCount(
-                //     crossAxisCount: 1),
-                itemBuilder: (context, index) {
-                  return
-                    GestureDetector(
-                      onTap:
-                          () {
-                            if (profileGetController.routing[index] != null) {
-                              Get.to(() => profileGetController.routing[index]!);
-                            }
-                          },
-
-                      child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child:
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Image.asset(
-                              profileGetController.options.keys.elementAt(index)),
-                          // Icon(
-                          //     profileGetController.options.keys.elementAt(index), size: 70, color: AppColors.tertiaryColor[800],),
-                          SizedBox(width: 20,),
-                          Text(profileGetController.options.values
-                              .elementAt(index), style: TextStyle(fontSize: 18, color: AppColors.tertiaryColor),),
-                        ],
-                      ),
-                      // SizedBox(
-                      //   width: Get.width/4,
-                      //   height: Get.height/4,
-                      //   child: NeumorphicButton(
-                      //     onPressed:
-                        //     () {
-                      //       if (profileGetController.routing[index] != null) {
-                      //         Get.to(() => profileGetController.routing[index]!);
-                      //       }
-                      //     },
-                      //     style: const NeumorphicStyle(
-                      //       shape: NeumorphicShape.convex,
-                      //       color: Colors.white,
-                      //       depth: 2,
-                      //       intensity: 0.6,
-                      //     ),
-                      //     child:
-                      //     Column(
-                      //       mainAxisAlignment: MainAxisAlignment.center,
-                      //       mainAxisSize: MainAxisSize.min,
-                      //       children: [
-                      //         Icon(
-                      //             profileGetController.options.keys.elementAt(index)),
-                      //         Text(profileGetController.options.values
-                      //             .elementAt(index)),
-                      //       ],
-                      //     ),
-                      //   ),
-                      // ),
-                  ),
-                    );
-                },
-                itemCount: profileGetController.options.length,
+            Neumorphic(
+              style: NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                color: Colors.white,
+                depth: 1,
+                intensity: 0.6,
               ),
+              child:
+              Container(
+                  width: Get.width,
+                  height: Get.height,
+                child: GridView.builder(
+                  gridDelegate:
+                  const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 5.0,
+                    mainAxisSpacing: 5.0,
+                    childAspectRatio: 3
+                  ),
+                  // const SliverGridDelegateWithFixedCrossAxisCount(
+                  //     crossAxisCount: 1),
+                  itemBuilder: (context, index) {
+                    return
+                      GestureDetector(
+                        onTap:
+                            () {
+                              if (profileGetController.routing[index] != null) {
+                                Get.to(() => profileGetController.routing[index]!);
+                              }
+                            },
+
+                        child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child:
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            // Image.asset(
+                            //     profileGetController.options.keys.elementAt(index)),
+                            Icon(
+                                profileGetController.options.keys.elementAt(index), size: 40, color: AppColors.secondaryColor[500],),
+                            SizedBox(width: 20,),
+                            Text(profileGetController.options.values
+                                .elementAt(index), style: TextStyle(fontSize: 18, color: AppColors.secondaryColor[500]),),
+                          ],
+                        ),
+                        // SizedBox(
+                        //   width: Get.width/4,
+                        //   height: Get.height/4,
+                        //   child: NeumorphicButton(
+                        //     onPressed:
+                          //     () {
+                        //       if (profileGetController.routing[index] != null) {
+                        //         Get.to(() => profileGetController.routing[index]!);
+                        //       }
+                        //     },
+                        //     style: const NeumorphicStyle(
+                        //       shape: NeumorphicShape.convex,
+                        //       color: Colors.white,
+                        //       depth: 2,
+                        //       intensity: 0.6,
+                        //     ),
+                        //     child:
+                        //     Column(
+                        //       mainAxisAlignment: MainAxisAlignment.center,
+                        //       mainAxisSize: MainAxisSize.min,
+                        //       children: [
+                        //         Icon(
+                        //             profileGetController.options.keys.elementAt(index)),
+                        //         Text(profileGetController.options.values
+                        //             .elementAt(index)),
+                        //       ],
+                        //     ),
+                        //   ),
+                        // ),
+                    ),
+                      );
+                  },
+                  itemCount: profileGetController.options.length,
+                ),
+              ),
+              //   const ListTile(
+              //
+              //       title: Text(
+              //         "Track orders",
+              //         style: TextStyle(
+              //             color: Colors.black38, fontWeight: FontWeight.w600),
+              //       ),
+              //
+              //       trailing:
+              //       Icon(
+              //                        Icons.arrow_forward_ios, size: 40, color: Colors.black38,),
+              //
+              // ),
+
             ),
+
           ),
           bottomNavigationBar: Padding(
             padding: const EdgeInsets.all(4.0),
             child: Neumorphic(
               style: NeumorphicStyle(
                 shape: NeumorphicShape.flat,
-                color: AppColors.primaryColor,
+                color: AppColors.secondaryColor[600],
                 depth: 1,
                 intensity: 0.6,
               ),
-              child: ListTile(
+              child:
+              ListTile(
                   title: const Text(
                     "Subscribe to NewsLetter",
                     style: TextStyle(
